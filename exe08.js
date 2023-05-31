56fa6463a16a27e502200f1d67956d9d8124f699
Murilo Ferreira <115593380+Murilo57@users.noreply.github.com>
2023-05-31 17:22:13 -0300

    Three Commit
    Aula 11

---
+++ exe08.js
@@ -0,0 +1,8 @@
+
+var speed = 60.5
+console.log(`its speed to the car is ${speed}Km/h`)
+if(speed > 80){
+    console.log(`Your ultrapassing the speed allowed. Multed!`) 
+}else{
+console.log(`Always drive utilizing belt of security`)
+}
