3b6a6e3781877b418658b408a9746ec6271d3130
Murilo Ferreira <115593380+Murilo57@users.noreply.github.com>
2023-05-31 22:16:35 -0300

    Four Commit
    added exercicies of 09 to the exe11

---
+++ exe09.js
@@ -0,0 +1,8 @@
+//Condição composta
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais != Brasil){
+    console.log('Your is estranged')
+} else {
+    console.log('Your is brazilian')
+}
