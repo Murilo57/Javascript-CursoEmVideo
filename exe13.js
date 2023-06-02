50013b9e3443854047d5443036402ae7177fc3ff
Murilo Ferreira <115593380+Murilo57@users.noreply.github.com>
2023-06-02 09:52:39 -0300

    exe11
    exercises from 11 to 13

---
+++ exe13.js
@@ -0,0 +1,42 @@
+var now = new Date()
+var dayweek = now.getDay()
+
+/*
+Day of the week:
+
+0 - Sunday
+1 - Monday
+2 - Tuesday
+3 - Wednesday
+4 - Thursday
+5 - Friday
+6 - Saturday
+*/
+console.log(dayweek)
+
+switch(dayweek) {
+    case 0:
+        console.log('Sunday')
+        break
+    case 1:
+        console.log('Monday')    
+        break
+    case 2:
+        console.log('Tuesday')    
+        break
+    case 3:
+        console.log('Wednesday')
+        break
+    case 4:
+        console.log('Thursday')
+        break        
+    case 5:
+        console.log('Friday')    
+        break
+    case 6:
+        console.log('Saturday')
+        break
+    default:
+        console.log('[ERRO] Invalid Day!')
+        break    
+}
