fd850eeb05c19aa7ff2c89ca523a009eee65ed41
Murilo Ferreira <115593380+Murilo57@users.noreply.github.com>
2023-06-22 15:41:37 -0300

    Update objeto01.js

--- Aula17/objeto01.js
+++ Aula17/objeto01.js
@@ -3,7 +3,7 @@ let amigo = {nome:'José',
 sexo: 'M',
 peso: 85.4,
 engorda(p=0){
-    console.log(engordou)
+    console.log('engordou')
     this.peso += p
 }}
 
@@ -11,3 +11,4 @@ amigo.engorda(2)
 console.log(`${amigo.nome} pesa ${amigo.peso}KG`)
 
 //Uma array também é considerada um objeto em JavaScript pois são estruturas da mesma classe
+
